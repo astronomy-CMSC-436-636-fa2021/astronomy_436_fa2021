6f2a40c14f66b1d44a61190906e114105ffc3439
dmercanti <dmercanti821@outlook.com>
2021-11-11 06:08:45 -0500

    Working (?) cumulative photos

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from astropy.visualization import astropy_mpl_style
 from astropy.utils.data import get_pkg_data_filename
 from astropy.io import fits
 from astropy.table import Table
+from astropy.table import vstack
 from astropy.utils.data import download_file
 import numpy as np
 from astropy.io import fits
@@ -55,47 +56,62 @@ def main():
     start_week, end_wk = do_some_prompts()
     # now we go out and download the files
     download_some_files(start_week,end_wk)
-
+    full_evt_data = list()
     #user_input=input("enter fit file\n")
     for i in range (start_week, end_wk+1):
         file_to_open = str(Path().absolute()) + "\\lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits"
         image_file=fits.open(file_to_open, memmap= True)
         #image_file.info() debug info about the FITS
-        evt_data=Table(image_file[1].data)
-        fig, ax = plt.subplots(1, figsize=(10, 10))
-        #test_dat = evt_data[0:100]  # subset of data for quickness
-        # test_dat_max = max(test_dat["ENERGY"])
-        act_dat_max = max(evt_data["ENERGY"])
-        # ax.scatter(test_dat['RA'], test_dat['DEC'], c=test_dat['ENERGY']/test_dat_max, s=.001 )
-        ax.scatter(evt_data['RA'], evt_data['DEC'], c=1 / evt_data['ENERGY'], s=.01)
+        full_evt_data.append(Table(image_file[1].data)) #add opened table to collection
+        evt_data = full_evt_data[i - start_week] # give it evt_data name for backwards compatability
 
 
-        # showing scatter plot
-        #energy_hist = plt.scatter(evt_data["RA"],evt_data['ENERGY'])
+        #np.sum(ii)
+        #NBINS = (500,500)
+        #img_zero, yedges, xedges = np.histogram2d(evt_data['RA'][ii], evt_data['DEC'][ii], NBINS)
+        # extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+        #plt.imshow(img_zero)
         #plt.show()
-        # showing 2 d histagram
+
+        # showing plt hist2d
+        nbins = (100,100)
+        fig, ax = plt.subplots(1)
         ii = np.in1d(evt_data['CONVERSION_TYPE'], [0, 1])
-        np.sum(ii)
-        NBINS = (500,500)
-        img_zero, yedges, xedges = np.histogram2d(evt_data['RA'][ii], evt_data['DEC'][ii], NBINS)
-        #extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-
-        plt.imshow(img_zero)
-        plt.show()
-
-        #showing plt hist2d
-        NBINS = (100,100)
-        img_zero_mpl = plt.hist2d(evt_data['RA'][ii], evt_data['DEC'][ii], NBINS, cmap='viridis', norm=LogNorm())
+        img_zero_mpl = plt.hist2d(evt_data['RA'][ii], evt_data['DEC'][ii], nbins, cmap='viridis', norm=LogNorm())
         cbar = plt.colorbar(ticks=[1.0,3.0,6.0])
         cbar.ax.set_yticklabels(['1','3','6'])
+        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
         plt.xlabel('RA')
         plt.ylabel('DEC')
         plt.show()
-        img_one_mpl = plt.hist2d(evt_data['L'][ii], evt_data['B'][ii], NBINS, cmap='viridis', norm=LogNorm())
+        fig, ax = plt.subplots(1)
+        img_one_mpl = plt.hist2d(evt_data['L'][ii], evt_data['B'][ii], nbins, cmap='viridis', norm=LogNorm())
         cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
         cbar.ax.set_yticklabels(['1', '3', '6'])
+        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
         plt.xlabel('L')
         plt.ylabel('B')
         plt.show()
-
+    concat_table = vstack(full_evt_data)
+    ii = np.in1d(concat_table['CONVERSION_TYPE'], [0, 1])
+    NBINS = (100, 100)
+    # Start on collective rad/dec
+    fig, ax = plt.subplots(1)
+    img_zero_mpl = plt.hist2d(concat_table['RA'][ii], concat_table['DEC'][ii], NBINS, cmap='viridis', norm=LogNorm())
+    cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
+    cbar.ax.set_yticklabels(['1', '3', '6'])
+    plt.title("Time Averaged from week" + int_to_string(start_week) + "to" + int_to_string(end_wk))
+    plt.xlabel('RA')
+    plt.ylabel('DEC')
+    plt.show()
+    # start on collection L/B
+    fig, ax = plt.subplots(1)
+    img_one_mpl = plt.hist2d(concat_table['L'][ii], concat_table['B'][ii], NBINS, cmap='viridis', norm=LogNorm())
+    cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
+    cbar.ax.set_yticklabels(['1', '3', '6'])
+    plt.title("Time Averaged from week " + int_to_string(start_week) + " to " + int_to_string(end_wk))
+    plt.xlabel('L')
+    plt.ylabel('B')
+    plt.show()
 main()
