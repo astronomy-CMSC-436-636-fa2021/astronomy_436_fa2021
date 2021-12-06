d50d9a1a48562985e33ff45623e9b7d3ed873f98
dmercanti <dmercanti821@outlook.com>
2021-12-05 20:25:01 -0500

    Placed histograms in separate functions to make code easier to modify/repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from astropy.visualization import astropy_mpl_style
-#plt.style.use(astropy_mpl_style)
+# plt.style.use(astropy_mpl_style)
 from astropy.utils.data import get_pkg_data_filename
 from astropy.io import fits
 from astropy.table import Table
@@ -14,142 +14,175 @@ import os
 import urllib
 import ssl
 from pathlib import Path
+
+full_evt_data = []
+full_sum_energy = 0
+start_week = 0
+end_week = 0
+full_weights = []
+
+
 def do_some_prompts():
-    #Todo - make this better prompt, probably with GUI
-    #in this file, I want to prompt the user for a date range
+    # Todo - make this better prompt, probably with GUI
+    # in this file, I want to prompt the user for a date range
     okay = False
     while (not (okay)):
         # todo: convert to a GUI picking actual weeks rather than numerically
         week_start = int(input("What week would you like to start (009 minimum)"))
         week_end = int(input("What week would you like to end? (700 max)"))
-        temp = input("There will be approximately {} MB for those files, is that ok?(y/n)".format(60 * (week_end-week_start + 1) ))
+        temp = input("There will be approximately {} MB for those files, is that ok?(y/n)".format(
+            60 * (week_end - week_start + 1)))
         if (temp == 'y'):
             okay = True
-        return  week_start, week_end
+        #return week_start, week_end, 0, 0
     okay = False
     while (not (okay)):
-        print("Getting values for bounds of energy, measured in MeV")
+        print("Getting values for bounds of energy, measured in MeV. Enter 0 for both if you don't want to use Energy Filtering")
         energy_start = float(input("What is your lower bound for the energy? (Recommend no lower than 20)"))
         energy_end = float(input("What is your upper bound for the energy (Recommend no higher than 3500)"))
-        if (energy_end > energy_start):
+        if energy_end > energy_start or (energy_end == 0 and energy_start == 0):
             okay = True
+        else:
+            print("That range does not make sense, please put them in order.")
     return week_start, week_end, energy_start, energy_end
+
 
 def int_to_string(i):
     if (i <= 9):
         return "00" + str(i)
     if (i < 99):
-        return "0"+str(i)
+        return "0" + str(i)
     else:
         return str(i)
 
+
 def download_some_files(week_strt, week_end):
     dl_root = "https://heasarc.gsfc.nasa.gov/FTP/fermi/data/lat/weekly/photon/"
-    dl_path = os.path.join("fits","archive")
+    dl_path = os.path.join("fits", "archive")
     ssl._create_default_https_context = ssl._create_unverified_context
-    for i in range(week_strt, week_end+1):
-        #print()
-        dl_link = dl_root +"lat_photon_weekly_w{fname}_p305_v001.fits".format(fname=int_to_string(i))
-        #print(dl_link)
-        urllib.request.urlretrieve(dl_link,"lat_photon_weekly_w{}_p305_v001.fits".format(int_to_string(i)) )
-def main():
-    start_week, end_wk = do_some_prompts()
-    # now we go out and download the files
-    download_some_files(start_week,end_wk)
+    for i in range(week_strt, week_end + 1):
+        # print()
+        dl_link = dl_root + "lat_photon_weekly_w{fname}_p305_v001.fits".format(fname=int_to_string(i))
+        # print(dl_link)
+        urllib.request.urlretrieve(dl_link, "lat_photon_weekly_w{}_p305_v001.fits".format(int_to_string(i)))
+
+
+def procedural_gen_full(start_week, end_week, energy_start, energy_end):
     full_evt_data = list()
     total_sum = 0
-    #user_input=input("enter fit file\n")
-    for i in range (start_week, end_wk+1):
+    # user_input=input("enter fit file\n")
+    for i in range(start_week, end_week + 1):
         file_to_open = str(Path().absolute()) + "\\lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits"
-        image_file=fits.open(file_to_open, memmap= True)
-        #image_file.info() debug info about the FITS
-        full_evt_data.append(Table(image_file[1].data)) #add opened table to collection
-        evt_data = full_evt_data[i - start_week] # give it evt_data name for backwards compatability
-        # showing plt hist2d
-        nbins = (100,100)
-        fig, ax = plt.subplots(1)
-        ii = np.in1d(evt_data['CONVERSION_TYPE'], [0, 1])
-        img_zero_mpl = plt.hist2d(evt_data['RA'][ii], evt_data['DEC'][ii], nbins, cmap='viridis', norm=LogNorm())
-        cbar = plt.colorbar(ticks=[1.0,3.0,6.0])
-        cbar.ax.set_yticklabels(['1','3','6'])
-        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
-        plt.xlabel('RA')
-        plt.ylabel('DEC')
-        plt.show()
-        fig, ax = plt.subplots(1)
-        img_one_mpl = plt.hist2d(evt_data['L'][ii], evt_data['B'][ii], nbins, cmap='viridis', norm=LogNorm())
-        cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
-        cbar.ax.set_yticklabels(['1', '3', '6'])
-        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
-        plt.xlabel('L')
-        plt.ylabel('B')
-        plt.show()
-        # end occurence log plots, now try energy
-        sum_energy = 0
-        sum_energy = sum(evt_data['ENERGY'])
-        total_sum = total_sum + sum_energy
-        #print (sum_energy)
-        nbins = (100, 100)
-        fig, ax = plt.subplots(1)
-        ii = np.in1d(evt_data['CONVERSION_TYPE'], [0, 1])
-        img_two_mpl = plt.scatter(evt_data['RA'][ii], evt_data['DEC'][ii],c= evt_data['ENERGY']/sum_energy, s = .01)
-        #cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
-        #cbar.ax.set_yticklabels(['1', '3', '6'])
-        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
-        plt.xlabel('RA')
-        plt.ylabel('DEC')
-        plt.show()
-        fig, ax = plt.subplots(1)
-        img_three_mpl = plt.scatter(evt_data['L'][ii], evt_data['B'][ii],c= evt_data['ENERGY']/sum_energy, s = 0.01)
-        #cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
-        #cbar.ax.set_yticklabels(['1', '3', '6'])
-        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
-        plt.xlabel('L')
-        plt.ylabel('B')
-        plt.show()
-        evt_data = 0
+        image_file = fits.open(file_to_open, memmap=True)
+        my_table = Table(image_file[1].data)
+        image_file.close()
+        # dropping these rows makes the future sort much faster
+        del (my_table['THETA'])
+        del (my_table['PHI'])
+        del (my_table['ZENITH_ANGLE'])
+        del (my_table['EARTH_AZIMUTH_ANGLE'])
+        del (my_table['TIME'])
+        del (my_table['EVENT_ID'])
+        del (my_table['RUN_ID'])
+        del (my_table['RECON_VERSION'])
+        del (my_table['CALIB_VERSION'])
+        del (my_table['EVENT_CLASS'])
+        del (my_table['EVENT_TYPE'])
+        del (my_table['LIVETIME'])
+        del (my_table['DIFRSP0'])
+        del (my_table['DIFRSP1'])
+        del (my_table['DIFRSP2'])
+        del (my_table['DIFRSP3'])
+        del (my_table['DIFRSP4'])
+        # we are going to use the weights argument in hist2d to do the cutoffs
+        my_weights = my_table['ENERGY']
+        for loop_through_weights in range(len(my_weights)):
+            item = my_weights[loop_through_weights]
+            if energy_start == 0 or energy_end == 0:
+                continue
+            if item < energy_start or item > energy_end:
+                my_weights[loop_through_weights] = 0 # if the item is outside the range, we replace it with 0
 
-    # Start together data!
-    if (start_week == end_wk):
-        return 0 # don't bother with not other crap
+        full_weights.extend(my_weights)
+        #my_table.show_in_browser()
+        #myTable.remove_rows()
+        full_evt_data.append(my_table)  # add opened table to collection
+        hist_plot_occurrences(my_table, i,my_weights)
+        energy_hist(my_table, i,my_weights)
+    # now that those are done, we can do it for the full image
     concat_table = vstack(full_evt_data)
-    ii = np.in1d(concat_table['CONVERSION_TYPE'], [0, 1])
-    NBINS = (100, 100)
-    # Start on collective rad/dec
+    hist_plot_occurrences(concat_table, -1,full_weights)
+    energy_hist(concat_table, -1,full_weights)
+
+
+def hist_plot_occurrences(evt_data, i, my_weights):
+    # produces the plot_occurrences histogram, for Radial and Declination Coords
+    # image_file.info() debug info about the FITS
+    # starting plt hist2d -
+    my_start_week = start_week
+    my_end_week = end_week
+    nbins = (500, 500)
     fig, ax = plt.subplots(1)
-    img_zero_mpl = plt.hist2d(concat_table['RA'][ii], concat_table['DEC'][ii], NBINS, cmap='viridis', norm=LogNorm())
+    ii = np.in1d(evt_data['CONVERSION_TYPE'], [0, 1])
+    img_zero_mpl = plt.hist2d(evt_data['RA'][ii], evt_data['DEC'][ii], nbins, cmap='viridis', norm=LogNorm(), weights=np.ceil(np.tanh(my_weights)))
     cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
     cbar.ax.set_yticklabels(['1', '3', '6'])
-    plt.title("Time Averaged from week " + int_to_string(start_week) + " to " + int_to_string(end_wk))
-    plt.xlabel('RA')
-    plt.ylabel('DEC')
-    plt.show()
-
-    # start on collection L/B
-    fig, ax = plt.subplots(1)
-    img_one_mpl = plt.hist2d(concat_table['L'][ii], concat_table['B'][ii], NBINS, cmap='viridis', norm=LogNorm())
-    cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
-    cbar.ax.set_yticklabels(['1', '3', '6'])
-    plt.title("Time Averaged from week " + int_to_string(start_week) + " to " + int_to_string(end_wk))
-    plt.xlabel('L')
-    plt.ylabel('B')
-    plt.show()
-
-    fig, ax = plt.subplots(1)
-    #ii = np.in1d(evt_data['CONVERSION_TYPE'], [0, 1])
-    img_two_mpl = plt.scatter(concat_table['RA'][ii], concat_table['DEC'][ii], c=concat_table['ENERGY'] / total_sum, s=.01)
-    # cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
-    # cbar.ax.set_yticklabels(['1', '3', '6'])
-    plt.title("Time Averaged from week " + int_to_string(start_week) + " to " + int_to_string(end_wk))
+    if i < 0:
+        plt.title(f"Time Averaged from week {int_to_string(my_start_week)} to {int_to_string(my_end_week)}")
+    else:
+        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
     plt.xlabel('RA')
     plt.ylabel('DEC')
     plt.show()
     fig, ax = plt.subplots(1)
-    img_three_mpl = plt.scatter(concat_table['L'][ii], concat_table['B'][ii], c=concat_table['ENERGY'] / total_sum, s=0.01)
-    # cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
-    # cbar.ax.set_yticklabels(['1', '3', '6'])
-    plt.title("Time Averaged from week " + int_to_string(start_week) + " to " + int_to_string(end_wk))
+    img_one_mpl = plt.hist2d(evt_data['L'][ii], evt_data['B'][ii], nbins, cmap='viridis', norm=LogNorm(), weights=np.ceil(np.tanh(my_weights)))
+    cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
+    cbar.ax.set_yticklabels(['1', '3', '6'])
+    if i < 0:
+        plt.title(f"Time Averaged from week {int_to_string(start_week)} to {int_to_string(end_week)}")
+    else:
+        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
     plt.xlabel('L')
     plt.ylabel('B')
     plt.show()
+
+
+def energy_hist(evt_data, i, my_weights):
+    # uses the sum of energy to create a scatter plot with the scaling being the current event_data/sum
+    sum_energy = sum(evt_data['ENERGY'])
+    # print (sum_energy)
+    nbins = (500, 500)
+    fig, ax = plt.subplots(1)
+    ii = np.in1d(evt_data['CONVERSION_TYPE'], [0, 1])
+    img_two_mpl = plt.hist2d(evt_data['RA'][ii], evt_data['DEC'][ii],nbins,cmap='cividis', weights=my_weights/sum_energy, norm=LogNorm())
+    # cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
+    # cbar.ax.set_yticklabels(['1', '3', '6'])
+    if i < 0:
+        plt.title(f"Time Averaged from week {int_to_string(start_week)} to {int_to_string(end_week)}")
+    else:
+        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
+    plt.xlabel('RA')
+    plt.ylabel('DEC')
+    plt.show()
+    fig, ax = plt.subplots(1)
+    img_three_mpl = plt.hist2d(evt_data['L'][ii], evt_data['B'][ii],nbins,cmap='cividis', weights=my_weights/sum_energy,norm=LogNorm())
+    # cbar = plt.colorbar(ticks=[1.0, 3.0, 6.0])
+    # cbar.ax.set_yticklabels(['1', '3', '6'])
+    if i < 0:
+        plt.title(f"Time Averaged from week {int_to_string(start_week)} to {int_to_string(end_week)}")
+    else:
+        plt.title("lat_photon_weekly_w" + int_to_string(i) + "_p305_v001.fits")
+    plt.xlabel('L')
+    plt.ylabel('B')
+    plt.show()
+    evt_data = 0
+
+
+def main():
+    global start_week, end_week, start_eng_range, end_eng_range
+    start_week, end_week, start_eng_range, end_eng_range = do_some_prompts()
+    download_some_files(start_week, end_week)
+    procedural_gen_full(start_week, end_week, start_eng_range, end_eng_range)
+
+
 main()
